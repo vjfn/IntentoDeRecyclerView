556ca23f38e2565c211865e22ddb150c38430719
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-10-04 17:03:14 +0200

    Hacer metodo addFruta y comprobar que funcione y si no preguntar a J.A

--- app/src/main/java/com/example/intentoderecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/intentoderecyclerview/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.intentoderecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         }
         binding.rvFrutas.setHasFixedSize(true)
         binding.rvFrutas.itemAnimator=DefaultItemAnimator()
+        val decoration=DividerItemDecoration(this,RecyclerView.VERTICAL)
+        binding.rvFrutas.addItemDecoration(decoration)
     }
 
     private fun onItemSelected(fruta: Fruta) {
